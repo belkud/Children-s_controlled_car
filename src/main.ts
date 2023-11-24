7a4826a5a6a8de088844d0ffe076724b2f14ca56
belkud <vlad-belkud@yandex.ru>
2023-11-25 00:25:25 +0300

    gj

--- src/main.ts
+++ src/main.ts
@@ -26,41 +26,38 @@ let moveX = 0
 let moveY = 0
 
 //! Изменение дневной\ночной темы
-sun.addEventListener ('click', ()=>{
-  document.body.style.transition = .5 +'s'
-  document.body.style.background = 'black'
-  theme.style.color = 'aliceblue'
-  carWithLamp.style.opacity = '1'
-  carWithLamp3.style.opacity = '.6'
-  car.style.opacity = '1'
-  car.style.filter = 'none'
-  
-  sun.style.transform = `translate(0px, 120px)`
-  sun.style.transition = .5 + 's'
-  cloud.style.filter = `brightness(.6)`
-  moon.style.transform = `translate(0px, 60px)`
-  moon.style.transition = .5 + 's'
-  moon.style.display = 'block'
-   
+theme.addEventListener ('click', ()=>{
+  if (document.body.style.background == 'lightgrey') {
+    document.body.style.background = 'black'
+    document.body.style.transition = .5 +'s'
+    theme.style.color = 'aliceblue'
+    carWithLamp.style.opacity = '1'
+    carWithLamp3.style.opacity = '.6'
+    car.style.opacity = '1'
+    car.style.filter = 'none'
+    sun.style.transform = `translate(0px, 120px)`
+    sun.style.transition = .5 + 's'
+    cloud.style.filter = `brightness(.6)`
+    moon.style.transform = `translate(0px, 60px)`
+    moon.style.transition = .5 + 's'
+    moon.style.display = 'block'
+  } else {
+    document.body.style.background = 'lightgrey'
+    document.body.style.transition = .5 +'s'
+    theme.style.color = 'black'
+    carWithLamp.style.opacity = '0'
+    carWithLamp3.style.opacity = '0'
+    car.style.filter = ''
+    sun.style.opacity = '1'
+    sun.style.transform = `translate(0px, 60px)`
+    sun.style.transition = .5 + 's'
+    sun.style.display = 'block'
+    cloud.style.filter = `brightness(1)`
+    moon.style.transform = `translate(0px, 120px)`
+    moon.style.transition = .5 + 's'
+  }  
 })
-
-
-moon.addEventListener ('click', ()=>{
-  document.body.style.transition = .5 +'s'
-  document.body.style.background = 'lightgray'
-  theme.style.color = 'black'
-  carWithLamp.style.opacity = '0'
-  carWithLamp3.style.opacity = '0'
-  car.style.filter = ''
-  
-  sun.style.opacity = '1'
-  sun.style.transform = `translate(0px, 60px)`
-  sun.style.transition = .5 + 's'
-  sun.style.display = 'block'
-  cloud.style.filter = `brightness(1)`
-  moon.style.transform = `translate(0px, 120px)`
-  moon.style.transition = .5 + 's'
-})
+ 
 
 
 
