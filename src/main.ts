2fedaa5ce155366c34952acd13c43db55f5d5b8f
belkud <vlad-belkud@yandex.ru>
2025-07-14 14:26:11 +0300

    car_degrees

--- src/main.ts
+++ src/main.ts
@@ -294,45 +294,37 @@ const page1 = document.querySelector('.page1') as HTMLDivElement
   
 let deg = 0
 function rotateRight () { 
-  deg+=90
-  if (deg>=91|| deg<=360 ) {
-    deg=90
-  } 
+  deg=90
   carBox.style.rotate = deg + 'deg'
 }
 
 function rotateDown () {
-  deg+=90
-  if (deg>=181||deg<=90) {
-    deg=180
-  } 
-  carBox.style.rotate = deg + 'deg'
+  deg=180
+    carBox.style.rotate = deg + 'deg'
   }
-
-function rotateLeft () {
-  if (deg==0) {deg-=90}
-  else {deg=270}
+  
+  function rotateLeft () {
+  deg=270
   carBox.style.rotate = deg + 'deg'  
-  if (deg==-90) deg=270
 }
-
-// function rotateLeft2 () {
-//   deg+=90 
-//   if (deg==270){
-//     deg=360
-//   }
-// }
-
 
 function rotateUp () {
-  deg+=90
-  if (deg >=0|| deg<=90) {
-    deg=0
+  if (deg<=180) {
+    deg=0 
+    console.log(90);
   }
   
-
-  if (deg == 360) deg = 0
- 
+  
+  if (deg == 270) {
+    deg=360
+    console.log(270);
+    
+  } 
+  // setTimeout(() => {
+  //   deg=0 
+    
+  // }, 20);
+  
   carBox.style.rotate = deg + 'deg'
 }
 
@@ -350,165 +342,165 @@ function rotateUp () {
 
  
 
-//! 3 этапа загрузки HTML страницы
-// 1
-// document.addEventListener('DOMContentLoaded', ready)
-// function ready(){
-//   console.log('DOMContentLoaded');
+// //! 3 этапа загрузки HTML страницы
+// // 1
+// // document.addEventListener('DOMContentLoaded', ready)
+// // function ready(){
+// //   console.log('DOMContentLoaded');
+// // }
+
+// // 2
+// // window.onload = ()=> {
+// //  console.log('страница загружена');
+// // }
+// // 3
+// // window.onunload = ()=> {
+// //   console.log('вы покидаете страницу');
+// // }
+
+
+
+
+// const button = document.querySelector('#button') as HTMLButtonElement
+// // button.innerHTML = document.readyState;
+
+// // readyState - степень загрузки элемента
+
+// // loading - в процессе загрузки
+// // interactive - полностью прочитан
+// // complete - полностью прочитан и все ресурсы загружены
+
+  
+
+//   // console.log(document.readyState);
+  
+//   // document.addEventListener('readystatechange', ()=>{
+//   //   console.log(document.readyState);
+//   // })
+  
+  
+  
+//   //! конструтор
+//   function User100500 (name:string, age:number)  {
+//     this.name =name
+//     this.age = age
+//     this.job = 'worker'
+//     //  this.sayHi = alert('hello')
+//   }
+ 
+
+  
+//   const Vasia = new User100500 ('Vasia', 25)
+//   console.log(Vasia);
+
+//   Vasia.color = 'red'
+//   console.log(Vasia);
+  
+//   Vasia.age =29
+//   console.log(Vasia);
+  
+ 
+//   //! Метод document.write – один из наиболее древних  и ненужных методов (работает пока страничка грузится)
+
+
+// let promise1 = new Promise((resolve, reject)=> {
+//  resolve('Успешно! ');  
+// });
+
+// promise1.then((value)=>{
+//   console.log(value);
+// })
+
+// let promise2 = new Promise((resolve, reject)=> {
+//  reject('Произошла ошибка! ');  
+// });
+
+// promise2.catch((value)=>{
+//   console.log(value);
+// })
+
+// function loadScript(src) {
+//   let script = document.createElement('script');
+//   script.src = src;
+//   document.head.append(script);
+// }
+// console.log(document);
+
+
+
+
+
+
+// let true1 =new Promise((resolve,reject)=> {
+//   resolve ('truetrue')
+// })
+// true1.then((value)=>{
+//   console.log(value)
+// })
+
+
+
+// let false1 = new Promise((resolve, reject)=>{
+//   reject('falsefalse')
+// })
+// false1.catch((event)=> 
+// console.log(event)
+// )
+
+ 
+
+
+// function Device (brand:string, year:number) {
+//   this.brand = brand
+//   this.year = year
+//   this.color = 'white'
+//   this.country = 'China'
 // }
 
-// 2
-// window.onload = ()=> {
-//  console.log('страница загружена');
+// let notebook = new Device ('Amoudo', 2017)
+// console.log(notebook);
+
+// let telephone = new Device ('Realme', 2019)
+// console.log(telephone);
+
+
+
+
+
+
+
+
+
+
+// function Person (name:string, age:number, city:string) {
+//   this.name = name
+//   this.age = age
+//   this.city = city
+//   this.country = 'Russia'
+//   this.job = 'programmist'
 // }
-// 3
-// window.onunload = ()=> {
-//   console.log('вы покидаете страницу');
-// }
 
-
-
-
-const button = document.querySelector('#button') as HTMLButtonElement
-// button.innerHTML = document.readyState;
-
-// readyState - степень загрузки элемента
-
-// loading - в процессе загрузки
-// interactive - полностью прочитан
-// complete - полностью прочитан и все ресурсы загружены
-
-  
-
-  // console.log(document.readyState);
-  
-  // document.addEventListener('readystatechange', ()=>{
-  //   console.log(document.readyState);
-  // })
-  
-  
-  
-  //! конструтор
-  function User100500 (name:string, age:number)  {
-    this.name =name
-    this.age = age
-    this.job = 'worker'
-    //  this.sayHi = alert('hello')
-  }
- 
-
-  
-  const Vasia = new User100500 ('Vasia', 25)
-  console.log(Vasia);
-
-  Vasia.color = 'red'
-  console.log(Vasia);
-  
-  Vasia.age =29
-  console.log(Vasia);
-  
- 
-  //! Метод document.write – один из наиболее древних  и ненужных методов (работает пока страничка грузится)
-
-
-let promise1 = new Promise((resolve, reject)=> {
- resolve('Успешно! ');  
-});
-
-promise1.then((value)=>{
-  console.log(value);
-})
-
-let promise2 = new Promise((resolve, reject)=> {
- reject('Произошла ошибка! ');  
-});
-
-promise2.catch((value)=>{
-  console.log(value);
-})
-
-function loadScript(src) {
-  let script = document.createElement('script');
-  script.src = src;
-  document.head.append(script);
-}
-console.log(document);
-
-
-
-
-
-
-let true1 =new Promise((resolve,reject)=> {
-  resolve ('truetrue')
-})
-true1.then((value)=>{
-  console.log(value)
-})
-
-
-
-let false1 = new Promise((resolve, reject)=>{
-  reject('falsefalse')
-})
-false1.catch((event)=> 
-console.log(event)
-)
-
- 
-
-
-function Device (brand:string, year:number) {
-  this.brand = brand
-  this.year = year
-  this.color = 'white'
-  this.country = 'China'
-}
-
-let notebook = new Device ('Amoudo', 2017)
-console.log(notebook);
-
-let telephone = new Device ('Realme', 2019)
-console.log(telephone);
-
-
-
-
-
-
-
-
-
-
-function Person (name:string, age:number, city:string) {
-  this.name = name
-  this.age = age
-  this.city = city
-  this.country = 'Russia'
-  this.job = 'programmist'
-}
-
-let user1 = new Person('Sveta', 27, 'Sochi')
-console.log(user1);
+// let user1 = new Person('Sveta', 27, 'Sochi')
+// console.log(user1);
 
 
  
 
-//! promise
-const prom1 = new Promise((resolve,reject)=>{
-    resolve ('Проверка пройдена')
-})
+// //! promise
+// const prom1 = new Promise((resolve,reject)=>{
+//     resolve ('Проверка пройдена')
+// })
 
-prom1.then((value)=>{
-    console.log(value)
-})
+// prom1.then((value)=>{
+//     console.log(value)
+// })
 
-const prom2 = new Promise((resolve,reject)=> {
-  reject ('Проверка не пройдена')
-})
+// const prom2 = new Promise((resolve,reject)=> {
+//   reject ('Проверка не пройдена')
+// })
 
-prom2.catch((value)=>{
-  console.log(value)
-})
+// prom2.catch((value)=>{
+//   console.log(value)
+// })
 
  
